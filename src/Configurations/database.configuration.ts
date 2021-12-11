eafe18712e264340244a54d9a48a0e4ea0895213
ahmed dousa <aa.dousa@gmail.com>
2021-12-11 21:19:25 +0400

    remove entities and rename database name

--- src/Configurations/database.configuration.ts
+++ src/Configurations/database.configuration.ts
@@ -6,8 +6,6 @@ export default registerAs('database', () => ({
   port: process.env.DB_PORT || 3306,
   username: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || 'root',
-  name: process.env.DB_NAME || 'test',
-  entities: ['dist/**/*.entity{ .ts,.js}'],
-  // autoLoadEntities: true,
+  database: process.env.DB_NAME || 'test',
   synchronize: process.env.APP_MODE === 'DEVELOPMENT',
 }));
