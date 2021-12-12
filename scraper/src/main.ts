5dacaf8a49d9e1cab8f32addbe373c6a025c1274
ahmed dousa <aa.dousa@gmail.com>
2021-12-12 23:07:04 +0400

    add tcp port options

--- scraper/src/main.ts
+++ scraper/src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import {
+  MicroserviceOptions,
+  Transport,
+  TcpOptions,
+} from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-      AppModule,
-      {
-        transport: Transport.TCP,
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '0.0.0.0',
+        port: process.env.PORT || 3001,
       },
+    } as TcpOptions,
   );
   await app.listen();
 }
