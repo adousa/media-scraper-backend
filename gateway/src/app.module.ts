e0ce8a3ca4b43458f0f045e4eb3ca80ace9c7504
ahmed dousa <aa.dousa@gmail.com>
2021-12-13 00:41:06 +0400

    add common configuration and microservice

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -4,9 +4,13 @@ import { MediaController } from './Controllers/media.controller';
 import { MediaService } from './Services/media.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import DatabaseConfiguration from './Configurations/database.configuration';
+import MicroserviceConfiguration from './Configurations/microservice.configuration';
+import CommonConfiguration from './Configurations/common.configuration';
+
 import { Connection } from 'typeorm';
-import { MediaRepository } from './Repositories/media.repository';
+import { MediaRepository } from './Repositories/url.repository';
 
 @Module({
   imports: [
@@ -21,7 +25,11 @@ import { MediaRepository } from './Repositories/media.repository';
     }),
     ConfigModule.forRoot({
       cache: true,
-      load: [DatabaseConfiguration],
+      load: [
+        DatabaseConfiguration,
+        MicroserviceConfiguration,
+        CommonConfiguration,
+      ],
     }),
   ],
   controllers: [MediaController],
