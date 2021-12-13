1140785ab98e31f519d5480c46f9f04e4eddc756
ahmed dousa <aa.dousa@gmail.com>
2021-12-13 15:11:09 +0400

    add scraper_service env variables

--- gateway/src/Configurations/microservice.configuration.ts
+++ gateway/src/Configurations/microservice.configuration.ts
@@ -1,4 +1,5 @@
 import { registerAs } from '@nestjs/config';
+import { Transport } from '@nestjs/microservices';
 
 export const SCRAPER_SERVICE = 'SCRAPPER_SERVICE';
 
@@ -7,4 +8,7 @@ export const SCRAPER_SERVICE_SCRAP_URL = 'SCRAPER_SERVICE_SCRAP_URL';
 export default registerAs('microservices_patterns', () => ({
   SCRAPER_SERVICE,
   SCRAPER_SERVICE_SCRAP_URL,
+  scraper_service_host: process.env.SCRAPER_SERVICE_HOST || '0.0.0.0',
+  scraper_service_port: process.env.SCRAPER_SERVICE_PORT || 3001,
+  scraper_service_transport: Transport.TCP,
 }));
