e87887e96dabbe8efa119ec44553b64a6c003499
ahmed dousa <ahmed.dousa@mazruiinternational.ae>
2021-12-13 17:03:19 +0400

    change port to be an env variable

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // enable validation for all requests
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
