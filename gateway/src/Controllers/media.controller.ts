017af2eec4534d4747511715ce161b7b9c94ccf3
ahmed dousa <ahmed.dousa@mazruiinternational.ae>
2021-12-13 17:29:41 +0400

    remove unused variable

--- gateway/src/Controllers/media.controller.ts
+++ gateway/src/Controllers/media.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { MediaService } from '../Services/media.service';
 import { CreateMediaDto } from './dto';
 import { Url } from '../Entities/url.entity';
