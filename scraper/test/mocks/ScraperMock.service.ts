b64a6da2615504a263bb6b8636a1514cc8375c77
ahmed dousa <ahmed.dousa@mazruiinternational.ae>
2021-12-14 21:10:21 +0400

    fix text case as endpoint response structure changed

--- scraper/test/mocks/ScraperMock.service.ts
+++ scraper/test/mocks/ScraperMock.service.ts
@@ -7,4 +7,13 @@ export class ScraperMockService {
       'blob:https://www.youtube.com/8014cc26-05a5-4247-84cf-d25894e5a592',
     ];
   }
+
+  async scrapUrl(url: string, types: string[]) {
+    return {
+      pageTitle: 'ALica Song',
+      img: [
+        'blob:https://www.youtube.com/8014cc26-05a5-4247-84cf-d25894e5a592',
+      ],
+    };
+  }
 }
